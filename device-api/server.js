7056073157c3abd4681b9a6eb4935c4fc6c3d664
nhuthuy <nhuthuy2972@gmail.com>
2020-06-22 08:52:58 +0700

    delete device-api

--- device-api/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const app = require('./app')
-
-const server = http.createServer(app)
-const port = process.env.PORT || 8888
-
-server.listen(port, () =>
-  console.log(`Server listening on http://localhost:${port}`)
-)
